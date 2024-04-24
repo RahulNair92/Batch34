ca8ecb5d1099ec5bb54ef1860404013f60e8325b
Vishnu Kartha <vishnukarthaa@gmail.com>
2024-04-23 22:24:13 -0400

    Added SkillsPage Pageclass and AddSkillTest test class to add a new skill.

---
+++ src/test/java/ca/qaguru/oranghrmbatch27/pages/SkillsPage.java
@@ -0,0 +1,57 @@
+package ca.qaguru.oranghrmbatch27.pages;
+
+import ca.qaguru.oranghrmbatch27.library.PageBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class SkillsPage extends PageBase{
+
+    WebDriver driver;
+    public SkillsPage(WebDriver _driver){
+        super(_driver);
+        driver = _driver;
+    }
+
+    public String adminClickXpath = "//span[text()='Admin']";
+    public String qualificationsClickXpath = "//span[text()='Qualifications ']";
+    public String skillsLinkXpath = "//a[text()='Skills']";
+    public String addButtonXpath = "//button[@type='button'][@class='oxd-button oxd-button--medium oxd-button--secondary']";
+    public String nameFieldXpath = "(//input[@class='oxd-input oxd-input--active'])[2]";
+    public String descriptionFieldXpath = "//textarea[@placeholder='Type description here']";
+
+    //public String saveButtonXpath = "//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']";
+    public String saveButtonId = "button[class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']";
+    public String checkSkillPartialXpath = "//div[@style='flex: 2 1 0%;']/following::div[text()='";
+
+    public void navigateToSkills(){
+        click(By.xpath(adminClickXpath));
+        click(By.xpath(qualificationsClickXpath));
+        click(By.xpath(skillsLinkXpath));
+    }
+
+    public void addAndSaveSkill(String skillName, String skillDescription) {
+        String checkskillXpath = checkSkillPartialXpath + skillName + "']";
+        if(!isElementVisible(By.xpath(checkskillXpath))){
+            click(By.xpath(addButtonXpath));
+            setText(By.xpath(nameFieldXpath), skillName);
+            setText(By.xpath(descriptionFieldXpath), skillDescription);
+            click(By.cssSelector(saveButtonId));
+
+        }
+        else{
+            System.out.println("Skill already present");
+        }
+
+    }
+
+    public void checkNewSkill(String skillName, String skillDescription){
+        String checkskillXpath = checkSkillPartialXpath + skillName + "']";
+        String checkdescripXPath = checkSkillPartialXpath + skillDescription + "']";
+        Assert.assertTrue(isElementVisible(By.xpath(checkskillXpath)));
+        Assert.assertTrue(isElementVisible(By.xpath(checkdescripXPath)));
+
+    }
+
+}
