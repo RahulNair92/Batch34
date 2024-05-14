04b641e43eb98fbde77a2e1b90e444cbc481dc4e
Siny <sinydaniel93@gmail.com>
2024-05-13 23:21:25 -0400

    Added page class and test class for Edit Language Scenario.

--- src/test/java/ca/qaguru/oranghrmbatch27/pages/LanguagesPage.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/pages/LanguagesPage.java
@@ -30,6 +30,14 @@ public class LanguagesPage extends PageBase {
     private final String lblAlreadyExistsMessage = "//div[@class='oxd-form-row'] /div";
     private final String ClassLanguageTable = "//div[@class='oxd-table-body'] /div[@class='oxd-table-card']";
     private final String verifyLanguageList = "//div[@class='oxd-table-cell' and @role='cell' and contains(@style, 'flex-basis: 80%')]//div[@class='data' and text()='']";
+    private final String editLanguageBtn = "(//i[contains(@class,'pencil')])[1]";
+    private final String editLanguageNameFld = "//div[@class='oxd-input-group__label-wrapper']//following::input";
+    private final String editLanguageSaveBtn = "//button[@type='submit']";
+    private final String editLanguageCancelBtn = "//button[@class='oxd-button oxd-button--medium oxd-button--ghost']";
+    private final String languagesTable = "oxd-table-body";
+    private final String deleteLanguageBtn = "(//i[contains(@class,'oxd-icon bi-trash')])[1]";
+    private final String confirmDelete = "//i[@class='oxd-icon bi-trash oxd-button-icon']";
+
 
     public void addNewLanguage(String newLanguageName) {
         click(By.xpath(languagesAddBtn));
@@ -50,5 +58,27 @@ public class LanguagesPage extends PageBase {
         }
 
     }
+
+
+    public void editLanguage() {
+        click(By.xpath(editLanguageBtn));
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(editLanguageNameFld)));
+
+        WebElement languageField = driver.findElement(By.xpath(editLanguageNameFld));
+        languageField.clear();
+        languageField.sendKeys("-Sample Edit");
+
+        WebElement alreadyExistsMessage = driver.findElement(By.xpath(lblAlreadyExistsMessage));
+        if (alreadyExistsMessage.getText().contains("Already exists")) {
+            click(By.xpath(editLanguageCancelBtn));
+        } else {
+            click(By.xpath(editLanguageSaveBtn));
+        }
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(languagesTable)));
+
+    }
+
 
 }
